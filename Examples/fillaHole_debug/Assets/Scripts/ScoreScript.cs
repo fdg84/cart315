59d434a8548c69382935d2ff9e7b95665f387acc
fdg84 <frankwell84@hotmail.com>
2024-03-07 20:47:39 -0800

    fillaHole DEBUG

---
+++ Examples/fillaHole_debug/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+    private int scoreText;
+    private int score;
+
+    public static ScoreScript S;
+    
+    // Start is called before the first frame update
+    void Start() {
+        score = (0.0f);
+        scoreText = GetComponent("Text");
+        DisplayScore();
+        S = this;
+    }
+
+    private void IncreaseScore() {
+        score++;
+        DisplayScore();
+    }
+    
+    private void DecreaseScore() {
+        score--;
+        DisplayScore();
+    }
+
+    public void DisplayScore() {
+        scoreText.text = "SCORE: " + score;
+    }
+}
